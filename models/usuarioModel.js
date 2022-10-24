8865dc6275c52d7bef8b3a8d0f382cce5585b145
fzura1 <felipe@zura.cl>
2022-10-24 13:20:35 -0300

    Update usuarioModel.js
    modelo usuario

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -2,4 +2,5 @@ module.exports={
     getUsuario:function(conexion,datos,funcion){
         conexion.query("SELECT * FROM USUARIOS where nombre=? and password=?",[datos.nombre,datos.password],funcion);
     },
+    
 }
