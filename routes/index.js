7b4760cd61419caf2316589ddc8a38a037dd62af
fzura1 <felipe@zura.cl>
2022-10-24 14:37:54 -0300

    rutas
    rutas actualizadas

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 const usuarioController=require('../controllers/usuarioController')
 
-router.get('/',function(req, res,next){
+router.get('biblioteca/',function(req, res,next){
     res.render('index', { title: 'Bienvenido',mjs:''});
 });
-router.post('/login',usuarioController.login);
-router.get('/cerrar_sesion',usuarioController.cerrar_sesion);
+router.post('biblioteca/login',usuarioController.login);
+router.get('biblioteca/cerrar_sesion',usuarioController.cerrar_sesion);
 
 module.exports = router;
--- routes/libro.js
+++ routes/libro.js
@@ -21,12 +21,12 @@ var cargarImg=multer({storage:rutaAlmacenImg});
 
 
 /* GET home page. */
-router.get('/',libroController.index);
-router.get('/agregar',libroController.agregar);
-router.post('/',cargarImg.single('imagen'),libroController.guardar_libro);
-router.post('/actualizar',cargarImg.single('imagen'),libroController.actualizar_libro);
-router.post('/eliminar/:id',libroController.eliminar_libro);
-router.get('/editar/:id',libroController.editar_libro);
+router.get('biblioteca/',libroController.index);
+router.get('biblioteca/agregar',libroController.agregar);
+router.post('biblioteca/',cargarImg.single('imagen'),libroController.guardar_libro);
+router.post('biblioteca/actualizar',cargarImg.single('imagen'),libroController.actualizar_libro);
+router.post('biblioteca/eliminar/:id',libroController.eliminar_libro);
+router.get('biblioteca/editar/:id',libroController.editar_libro);
 
 
 module.exports = router;
