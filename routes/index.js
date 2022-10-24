a33becc93d8825f6af4226f2fd9ad1020a38dcc5
fzura1 <felipe@zura.cl>
2022-10-24 10:45:21 -0300

    Update index.js
    ruta login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-/* GET home page. */
+const usuarioController=require('../controllers/usuarioController')
+
 router.get('/',function(req, res,next){
     res.render('index', { title: 'Bienvenido'});
 });
+router.post('/login',usuarioController.login);
 
 module.exports = router;
