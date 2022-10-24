7b334dfa87433f19dfbde3ee86ea0c70ec20a52d
fzura1 <felipe@zura.cl>
2022-10-24 13:20:13 -0300

    Update usuarioController.js
    login

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,28 +1,36 @@
 var con=require('../config/conexion');
 var usuario=require('../models/usuarioModel');
+var logs=require('../models/logsModel');
 module.exports ={
-    // metodo index de programador
-    index:function(req,res){
-        if(!req.session.nombre){
-            res.end("No tienes permiso de acceso");
-        }else{
-            //usa modelo usuario
-            usuario.getByIdUsuario(con,function(err,datos){
-                console.log(datos)
-                // renderiza vista index usuario
-                res.render('programador/index', { title: 'Login',usuario:datos });
-            });            
-        }
-    },
     login:function(req,res){
-        usuario.getUsuario(con,req.body,function(err,datos){
-            console.log(datos)
-            // res.redirect('/libro');
-        });
-        // usuario.getUsuario(con,function(err,datos){
-        //     console.log(datos)
-        //     // renderiza vista index libros
-        //     res.render('libro/index', { title: 'Inventario de Libros',libro:datos });
-        // });            
-    },    
+        var usuarios=req.body.nombre;
+        var claves=req.body.password;   
+        var validaEmail=/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        var validaClave=/^([A-Za-zÑñÁáÉéÍíÓóÚúÜü@$!?:.,;¿0-9\s]{1,50})$/;
+        if(!validaEmail.test(usuarios) || !validaClave.test(claves) ){
+            req.session.destroy();
+            res.render('index', { title: 'Bienvenido',mjs:"Usuario y/o clave con formato invalido" });
+        }else{
+            usuario.getUsuario(con,req.body,function(err,datos){
+                if(datos.length === 0){
+                    console.log("no existe");
+                    req.session.destroy();
+                    res.render('index', { title: 'Bienvenido',mjs:"Usuario y/o clave incorrecta" });
+                }else{
+                    console.log("existe");
+                    req.session.identificador = datos[0].id;
+                    req.session.nombre = datos[0].nombre;
+                    logs.setLog(con,req.session.identificador,'Sesión iniciada: '+req.session.nombre,function(err){
+                        console.log("almacena log modificado");
+                    });  
+                    res.redirect('/libro');
+                }  
+            });
+        }
+          
+    }, 
+    cerrar_sesion:function(req,res){
+        req.session.destroy();
+        res.render('index', { title: 'Bienvenido',mjs:"Sesión cerrada correctamente" });
+    }   
 }
