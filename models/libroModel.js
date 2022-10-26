818b06b25dc72df655daec9b6083daaf52d30276
fzura1 <felipe@zura.cl>
2022-10-26 11:20:58 -0300

    Update libroModel.js
    async modelo

--- models/libroModel.js
+++ models/libroModel.js
@@ -1,17 +1,17 @@
 module.exports={
-    getLibro:function(conexion,funcion){
-        conexion.query("SELECT * FROM LIBROS ",funcion);
+    getLibro:async function (conexion, funcion) {
+        conexion.query("SELECT * FROM LIBROS ", funcion);
     },
-    setLibro:function(conexion,datos,archivos,funcion){
+    setLibro:async function(conexion,datos,archivos,funcion){
         conexion.query("INSERT INTO LIBROS (titulo,autor,descripcion,imagen) values(?,?,?,?)",[datos.titulo,datos.autor,datos.descripcion,archivos.filename],funcion);
     },
-    getByIdLibro:function(conexion,id,funcion){
+    getByIdLibro: async function(conexion,id,funcion){
         conexion.query("SELECT * FROM LIBROS  where id=?",[id],funcion);
     },
-    deleteByIdLibro:function(conexion,id,funcion){
+    deleteByIdLibro:async function(conexion,id,funcion){
         conexion.query("DELETE FROM LIBROS  where id=?",[id],funcion);
     },
-    updateLibro:function(conexion,datos,archivos,funcion){
+    updateLibro:async function(conexion,datos,archivos,funcion){
         conexion.query("UPDATE LIBROS SET titulo=?, autor=?,descripcion=?,imagen=? WHERE id=?",[datos.titulo,datos.autor,datos.descripcion,archivos.filename,datos.id],funcion);
     },
 }
